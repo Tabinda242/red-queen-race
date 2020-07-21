766f78a98e31a85ff95c5b2ed8ce67bff01758d3
Tabinda242 <60565422+Tabinda242@users.noreply.github.com>
2020-07-22 02:03:03 +0500

    Create Red Queen Race Animation by using useEffect and useWebAnimation

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import RedQueenRace from './RedQueen/index';
+
+
+function App() {
+  return (
+    <div>
+      <RedQueenRace />
+    </div>
+  );
+}
+
+export default App;
